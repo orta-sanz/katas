f70835cd63c6d80ff915da4074395eee8b41554d
orta-sanz <orta.sanz.93@gmail.com>
2014-04-04 11:53:14 +0200

    Kata 4 de Scraping terminado, Qué bonito¡¡¡

---
+++ kata_4/script.js
@@ -0,0 +1,26 @@
+// Ejecución: node script.js
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+var url = 'http://tympanus.net/codrops/collective/collective-108';
+
+request(url, function (error, response, html) {
+	if (!error && response.statusCode == 200) {
+
+		var $ = cheerio.load(html);
+		var saco = {};
+
+		$('.ct-coll-item').find('article').each(function(index, cosa) {
+			if (index > 0) {
+				saco[index] = {'nombre': $(cosa).find('h2').text(), 'vinculo': $(cosa).find('.ct-coll-link').attr('href')}
+			}
+		})
+		
+		for(var i in saco) {
+			if (saco[i].vinculo != null) {
+				console.log(saco[i].nombre + ' : ' + saco[i].vinculo);
+			}
+		}
+	}
+})
