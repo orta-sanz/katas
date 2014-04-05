0e48d30dba51a354c6fb681bba4ec68b6f2980a9
orta-sanz <orta.sanz.93@gmail.com>
2014-04-05 13:51:45 +0200

    Modificado Kata 5

--- kata_5/router.js
+++ kata_5/router.js
@@ -1,9 +1,5 @@
-var manipuladorPeticion = require('./manipuladorPeticion');
-var manejador = {'/registrar': manipuladorPeticion.registrar};
-
 function route(path) {
 	console.log('Nos vamos a: ' + path);
-	manejador[path];
 }
 
 exports.route = route;
