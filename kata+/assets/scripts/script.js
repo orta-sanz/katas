516cfe4333f109eae196f491c9cce3e12d73d7ac
orta-sanz <orta.sanz.93@gmail.com>
2014-04-07 14:37:11 +0200

    En el Kata+ creados algunos assets

--- kata+/assets/scripts/script.js
+++
@@ -1,9 +0,0 @@
-var $ 	= require('jquery');
-
-$(document).ready(function() {
-	var trozos_enlace = ['http:','www.indiedb.com','games'];
-	$('#boton1').bind('click', function() {
-		var enlace = $('#url').val();
-		console.log(enlace.split('/'));
-	});
-});
--- kata+/gulpfile.js
+++ kata+/gulpfile.js
@@ -4,7 +4,7 @@ var browserify 	= require('gulp-browserify');
 var uglify 		= require('gulp-uglify');
 var handlebars 	= require('gulp-handlebars');
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts'] ['stylus']);
 
 gulp.task('scripts', function() {
 	return gulp.src('assets/scripts/script.js')
@@ -15,20 +15,20 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('js/'))
 });
 
-/*gulp.task('stylus', function() {
+gulp.task('stylus', function() {
 	return gulp.src('assets/css/*.styl')
 		.pipe(stylus({
 			import: ['nib'],
 			use: ['nib']
 		}))
 		.pipe(gulp.dest('css/'))
-});*/
+});
 
 /*gulp.task('plantilla', function() {
 	return gulp.src('assets/plantillas/*.hbs')
 		.pipe(gulp.dest('plantillas/'))
 })*/
 
-//gulp.watch('assets/css/*.styl', ['stylus']);
+gulp.watch('assets/css/*.styl', ['stylus']);
 gulp.watch('assets/scripts/script.js', ['scripts']);
 //gulp.watch('assets/plantillas/*.hbs' ['plantilla']);
