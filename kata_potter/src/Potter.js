c705556c23508d2e2dd222500a8aa75f20862555
orta-sanz <orta.sanz.93@gmail.com>
2014-04-10 08:39:44 +0200

    Primer commit Kata Potter

---
+++ kata_potter/src/Potter.js
@@ -0,0 +1,14 @@
+var carro 		= {cantidad: 0, articulos: [], total: 0};
+var descuentos	= [5,10,20,25];
+
+function devuelveCantidad() {
+	return carro.cantidad;
+}
+
+function devuelveArticulos() {
+	return carro.articulos.length;
+}
+
+function devuelveTotal() {
+	return carro.total;
+}
